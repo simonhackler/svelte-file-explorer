989c44141d21fc76b07e78b3842fa496f49695b6
Simon <simon>
2025-06-16 00:32:18 +0200

    retry

--- playwright.ci.config.ts
+++ playwright.ci.config.ts
@@ -2,12 +2,13 @@ import { defineConfig } from '@playwright/test';
 import shared from './playwright.config';
 
 export default defineConfig({
-  ...shared,
-  webServer: {
-    command: 'bun run build && bun run preview --port=5173',
-    port: 5173,
-    timeout: 120_000,
-    reuseExistingServer: false,
-  },
+    ...shared,
+    retries: 2,
+    webServer: {
+        command: 'bun run build && bun run preview --port=5173',
+        port: 5173,
+        timeout: 120_000,
+        reuseExistingServer: false,
+    },
 });
 
