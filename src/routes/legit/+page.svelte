fee7e0f4821ac91e1fd5a3a98f4e1959e5d2c44f
simon <simonhackler@gmail.com>
2026-02-21 16:24:35 +0100

    added

--- src/routes/legit/+page.svelte
+++ src/routes/legit/+page.svelte
@@ -9,17 +9,29 @@
 	const opfsAdapter = OPFSAdapter.create();
 
 	const legitFs = await openLegitFsWithMemoryFs();
+    let branches = await legitFs.promises.readdir('.legit/branches');
+    console.log(branches);
 	await legitFs.setCurrentBranch('anonymous');
 	await legitFs.promises.mkdir('/.legit/branches/main');
-	await legitFs.promises.writeFile('/hello.txt', 'Hello world');
-	const data = await legitFs.promises.readFile('/hello.txt', 'utf8');
-	console.log(data); // → "Hello world"
-	const res = await legitFs.promises.readdir('/', { withFileTypes: true, recursive: false });
-	console.log(res); // → [".legit", "hello.txt"]
+    branches = await legitFs.promises.readdir('.legit/branches');
+    console.log(branches);
+	await legitFs.promises.writeFile('.legit/branches/main/.hello.txt', 'Hello world');
+	const data = await legitFs.promises.readFile('.legit/branches/main/.hello.txt', 'utf8');
+    console.log("stat");
+    console.log(await legitFs.promises.stat('.legit/branches/main/.hello.txt'));
+	console.log("data", data); // → "Hello world"
+	const res = await legitFs.promises.readdir('/');
+	console.log("res", res); // → [".legit", "hello.txt"]
 
 	// inspect commit history (stored in .legit)
 	const history = await legitFs.promises.readFile('/.legit/branches/main/.legit/history', 'utf8');
-	console.log(history);
+	console.log("history", history);
+    console.log(await legitFs.promises.readdir('/'));
+
+    const statusContent = await legitFs.promises.readFile('.legit/status', 'utf8');
+    const status = JSON.parse(statusContent);
+    console.log(status.branch); // Current branch name
+    console.log(status.files); // Array of modified files
 </script>
 
 {#await opfsAdapter then opfsAdapter}
